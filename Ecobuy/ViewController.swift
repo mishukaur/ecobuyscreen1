c498fccc61b15f3c3ba6b4cacf8509799966fa05
mishukaur <69322074+mishukaur@users.noreply.github.com>
2020-08-11 14:26:54 -0400

    Add files via upload

---
+++ Ecobuy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ecobuy
+//
+//  Created by Manjot Kaur on 8/11/20.
+//  Copyright © 2020 kwk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
